9ce86ca805614971b693f4e473cae1a2cb260b09
Merins08 <m.marino.3566@vallauri.edu>
2025-05-01 10:28:28 +0200

    inzio es

--- index.js
+++ index.js
@@ -2,10 +2,25 @@
 
 const DIM = 4;
 
-window.onload=function(){
+window.addEventListener("load", function () {
+   GeneraTabella();
+});
 
+function GeneraTabella() {
+   const wrapper = document.getElementById("wrapper");
+   for (let i = 0; i < DIM; i++) {
+      for (let i = 0; i < DIM; i++) {
+         const div = document.createElement("div");
+         wrapper.appendChild(div);
+         div.classList.add("cella");
+         div.id = `div-${i}-${j}`;
+         
+      }
+   }
 }
 
-function generaNumero(min, max){
-   return Math.floor((max - min) * Math.random()) + min;   
+function esegui(event) { console.log(event.target.value) }
+
+function generaNumero(min, max) {
+   return Math.floor((max - min) * Math.random()) + min;
 }
